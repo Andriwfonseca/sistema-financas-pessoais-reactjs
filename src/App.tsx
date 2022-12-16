bce95c8e672bf5e486c64e5c70579b4a8c022795
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-12-15 22:16:46 -0300

    criação da parte de exibição dos itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
+import { Category } from './types/Category';
+import { Item } from './types/Item';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth));
+    console.log(filteredList, 'filteredList')
+    console.log(currentMonth)
+  },[list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
@@ -12,7 +29,7 @@ const App = () => {
 
         {/* Área de inserção */}
 
-        {/* Tabela de itens */}
+        <TableArea list={filteredList}></TableArea>
 
       </C.Body>
     </C.Container>
